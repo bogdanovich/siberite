fc67d32667aa747393de66d3196a33fc93f3b640
Anton Bogdanovich <27bogdanovich@gmail.com>
2015-10-25 16:22:46 -0700

    Add Consumer interface to Queue
    - Fix queue error messages

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -34,13 +34,13 @@ func Test_Open(t *testing.T) {
 
 	invalidQueueName := "%@#*(&($%@#"
 	q, err = Open(invalidQueueName, dir, &options)
-	assert.Equal(t, "Queue name is not alphanumeric", err.Error())
+	assert.EqualError(t, err, "queue: name is not alphanumeric")
 	q.Drop()
 
 	invalidQueueName = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
 		"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 	q, err = Open(invalidQueueName, dir, &options)
-	assert.Equal(t, "Queue name is too long", err.Error())
+	assert.EqualError(t, err, "queue: name is too long")
 	q.Drop()
 }
 
@@ -96,7 +96,7 @@ func Test_Peek(t *testing.T) {
 func testPeek(t *testing.T, q *Queue) {
 	inputValue := "1"
 	err = q.Enqueue([]byte(inputValue))
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 
 	for i := 0; i < 3; i++ {
 		item, err := q.Peek()
@@ -116,9 +116,7 @@ func Test_EnqueueDequeueLength(t *testing.T) {
 }
 
 func testEnqueueDequeueLength(t *testing.T, q *Queue) {
-	if q.Length() != 0 {
-		t.Error("Invalid initial length")
-	}
+	assert.NotEqual(t, 0, q.Length(), "Invalid initial length")
 
 	values := make([]string, 10)
 	for i := 0; i < 10; i++ {
@@ -135,6 +133,71 @@ func testEnqueueDequeueLength(t *testing.T, q *Queue) {
 		assert.Nil(t, err)
 		assert.Equal(t, values[i], string(item.Value), "Invalid value")
 	}
+}
+
+func Test_GetNext(t *testing.T) {
+	q, _ := Open(name, dir, &options)
+	testGetNext(t, q)
+	q.Drop()
+
+	q, _ = Open(name, dir, &optionsWithKeyPrefix)
+	testGetNext(t, q)
+	q.Drop()
+}
+
+func testGetNext(t *testing.T, q *Queue) {
+	values := []string{"1", "2"}
+	for i := 0; i < len(values); i++ {
+		q.Enqueue([]byte(values[i]))
+	}
+
+	value, err := q.GetNext()
+	assert.Nil(t, err)
+	assert.Equal(t, "1", string(value))
+
+	value, err = q.GetNext()
+	assert.Nil(t, err)
+	assert.Equal(t, "2", string(value))
+
+	value, err = q.GetNext()
+	assert.EqualError(t, err, "queue: is empty")
+}
+
+func Test_PutBack(t *testing.T) {
+	q, _ := Open(name, dir, &options)
+	testPutBack(t, q)
+	q.Drop()
+
+	q, _ = Open(name, dir, &optionsWithKeyPrefix)
+	testPutBack(t, q)
+	q.Drop()
+}
+
+func testPutBack(t *testing.T, q *Queue) {
+	values := []string{"1", "2"}
+	for i := 0; i < len(values); i++ {
+		q.Enqueue([]byte(values[i]))
+	}
+
+	err = q.PutBack([]byte("0"))
+	assert.EqualError(t, err, "queue: head can not be less then zero")
+
+	// get 1
+	q.GetNext()
+	// get 2
+	value, err := q.GetNext()
+	assert.NoError(t, err)
+
+	assert.Equal(t, uint64(2), q.Head())
+
+	err = q.PutBack(value)
+	assert.NoError(t, err)
+
+	assert.Equal(t, uint64(1), q.Head())
+
+	// Check that we get the same item with the next GetNext
+	value, _ = q.GetNext()
+	assert.Equal(t, "2", string(value))
 }
 
 func Test_GetItemByID(t *testing.T) {
@@ -162,7 +225,7 @@ func testGetItemByID(t *testing.T, q *Queue) {
 	assert.Equal(t, "3", string(item.Value))
 
 	item, err = q.GetItemByID(q.Head() + 5)
-	assert.Equal(t, "Id should be between head and tail", err.Error())
+	assert.Equal(t, "queue: out of bounds", err.Error())
 }
 
 func Test_GetItemByOffset(t *testing.T) {
@@ -190,7 +253,7 @@ func testGetItemByOffset(t *testing.T, q *Queue) {
 	assert.Equal(t, "3", string(item.Value))
 
 	item, err = q.GetItemByOffset(5)
-	assert.Equal(t, "Id should be between head and tail", err.Error())
+	assert.Equal(t, "queue: out of bounds", err.Error())
 }
 
 func Test_Prepend(t *testing.T) {
