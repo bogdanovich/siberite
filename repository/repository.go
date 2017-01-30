2286cd3943c5373fba471df138beba6942391df5
Anton Bogdanovich <ab@ab.local>
2017-01-29 20:13:01 -0800

    Bump version

--- repository/repository.go
+++ repository/repository.go
@@ -13,7 +13,7 @@ import (
 )
 
 // Version represents siberite version
-const Version = "siberite-0.6.2"
+const Version = "siberite-0.6.3"
 
 // QueueRepository represents a repository of queues
 type QueueRepository struct {
