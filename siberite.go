0a8395ed83464a24f77ef5f8f2dd1ba8ebdc8dd9
Neo <jeteon@users.noreply.github.com>
2017-04-03 19:49:28 +0200

    Add flag to allow specifying a PID file

--- siberite.go
+++ siberite.go
@@ -3,11 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
 	"os/signal"
 	"runtime"
+	"strconv"
 	"syscall"
 
 	service "github.com/bogdanovich/siberite/service"
@@ -16,6 +18,7 @@ import (
 var (
 	dataDir     = flag.String("data", "./data", "path to data directory")
 	hostAndPort = flag.String("listen", "0.0.0.0:22133", "ip and port to listen")
+	pidPath     = flag.String("pid", "", "path to PID file to use")
 	versionFlag = flag.Bool("version", false, "prints current version")
 )
 
@@ -30,6 +33,15 @@ func main() {
 		os.Exit(0)
 	}
 
+	// Write a PID file if its requested
+	if len(*pidPath) > 0 {
+		err := ioutil.WriteFile(*pidPath, []byte(strconv.Itoa(os.Getpid())), 0644)
+		if nil != err {
+			log.Fatalln(err)
+		}
+		defer os.Remove(*pidPath)
+	}
+	
 	laddr, err := net.ResolveTCPAddr("tcp", *hostAndPort)
 	if nil != err {
 		log.Fatalln(err)
